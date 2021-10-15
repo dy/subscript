5dd92c0639b78998733eb79bcf9bbc4985c18238
dy <df.creative@gmail.com>
2021-10-15 17:56:02 -0400

    Codegolf

--- subscript.js
+++ subscript.js
@@ -27,39 +27,26 @@ export const operators = {
 }
 
 // code → lispy tree
-export function parse (seq, od=operators) {
-  let op, c, v=[], g=[''], u=[],
-    ops = Object.keys(od).reverse(), op1=ops.filter(o=>o.length==1), op2=ops.filter(o=>o.length==2)
-    // og = [...op1,...op2].map(o=>o.replace(/./g,'\\$&')).join('|'),
-    // ore = new RegExp(`(^|${og})(${og})(${og})?`,'g')
+export function parse (seq, ops=operators) {
+  let op, c, v=[], g=[''], u=[], ord = Object.keys(ops).reverse(), uop=[], i=0
 
   // literals
   seq=seq
     .replace(/"[^"\\\n\r]*"|\b\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?\b/g, m => `#v${v.push(m[0]==='"'?m:parseFloat(m))-1}`)
     .replace(/\b(?:true|false|null)\b/g, m => `#v${v.push(m=='null'?null:m=='true')-1}`)
     .replace(/\s+/g,'')
-    // .replace(ore, (m,o0,o1,o2) => console.log(o0,o1)||`${o0}${u.push(o2?[o2,o1]:[o1])-1}@`) // up to 2 unary ops
 
   // unaries
-  let i = 0, buf='', uop=[]
-  for (; i < seq.length; ) {
-    let ch = seq[i]
-    // console.log(ch,g[0])
-    if (od[op=ch+seq[i+1]]||od[op=ch]) {
-      g[0]+=buf, buf=''
-      !uop ? (uop = [], g[0]+=op) : uop.unshift(op)
-      i+=op.length
+  for (; i < seq.length; g[0]+=c) {
+    // c = seq[i]
+    if (ops[c=seq[i]+seq[i+1]] || ops[c=seq[i]]) {
+      i+=c.length, uop ? (uop.unshift(c), c='') : (uop = [])
     }
     else {
-      if (uop) {
-        if (uop.length) g[0] += `${u.push(uop)-1}@`
-        uop = null
-      }
-      buf += ch, i++
+      if (uop?.length) g[0] += `${u.push(uop)-1}@`
+      uop = null, i++
     }
   }
-  g[0]+=buf
-  // console.log(g[0], u)
 
   // groups
   while(g.length!=c) c=g.length, g[0]=g[0]
@@ -69,7 +56,7 @@ export function parse (seq, od=operators) {
   // binaries w/precedence
   const oper = (s, l) => Array.isArray(s) ? [s.shift(), ...s.map(oper)]
     : s.includes(op) && (l=s.split(op)).length > 1 ? [op, ...l] : s
-  for (op of ops) g=g.map(oper)
+  for (op of ord) g=g.map(oper)
 
   // unwrap
   const deref = (s,c,r,i,m,n,un) => Array.isArray(s) ? [s.shift(), ...s.map(deref)]
