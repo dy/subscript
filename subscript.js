9b7ecbada2204b2688e2096462beadfd2c4e4779
dy <df.creative@gmail.com>
2021-10-19 16:09:04 -0400

    Rework ref group

--- subscript.js
+++ subscript.js
@@ -1,11 +1,11 @@
 // precedence order
 export const operators = {
   '!':(a,b)=>!b,
-  '~':(a,b)=>~b,
+  // '~':(a,b)=>~b,
   // '.':(a,b)=>console.log(a,b)||a[b], // TODO: meld in code
-  '.':(a,b)=>console.log(a,b)||b?a?.[b]:b,
+  // '.':(a,b)=>console.log(a,b)||b?a?.[b]:b,
   // '{':(a,b)=>console.log(a,b),
-  '**':(a,b)=>a**b,
+  // '**':(a,b)=>a**b,
   '*':(a,b)=>a*b,
   '/':(a,b)=>a/b,
   '%':(a,b)=>a%b,
@@ -17,7 +17,7 @@ export const operators = {
   '<=':(a,b)=>a<=b,
   '>':(a,b)=>a>b,
   '>=':(a,b)=>a>=b,
-  ' in ':(a,b)=>a in b,
+  // ' in ':(a,b)=>a in b,
   '==':(a,b)=>a==b,
   '!=':(a,b)=>a!=b,
   '&':(a,b)=>a&b,
@@ -29,79 +29,80 @@ export const operators = {
   ',':(a,b)=>b,
 }
 
-const br={'(':')','[':']','{':'}'}
-
 // code → lispy tree
-export function parse (seq, ops=operators) {
-  let op, c, v=[], u=[], opl = Object.keys(ops).reverse(), uop=[], i, res, cur = [], up, buf=''
+export function parse (seq, opf=operators) {
+  let op=[], b, c, v=[], u=[], g=[''], cur=[0], opl = Object.keys(opf).reverse(), i
 
-  // literals
+  // ref literals
   seq=seq
     .replace(/"[^"\\\n\r]*"|\b\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?\b/g, m => `#v${v.push(m[0]==='"'?m:parseFloat(m))-1}`)
     .replace(/\b(?:true|false|null)\b/g, m => `#v${v.push(m=='null'?null:m=='true')-1}`)
     // .replace(/\s+/g,'')
 
-  // groups
-  // FIXME: merge with unaries loop; create tree straight ahead
+  // ref groups/unaries
   for (i=0; i < seq.length;) {
     c=seq[i]
-    if (c=='\s'||c=='\r'||c=='\n'||c=='\t') i++, buf+=c
-    else if (ops[c=seq[i]+seq[i+1]] || ops[c=seq[i]]) {
-      i+=c.length, uop ? uop.unshift(c) : (uop = [], buf+=c)
+    if (c==' '||c=='\r'||c=='\n'||c=='\t') i++
+    else if (opf[c=seq[i]+seq[i+1]] || opf[c=seq[i]]) {
+      i+=c.length, !op? (op=[], g[cur[0]]+=c) : (g[cur[0]]+=op.unshift(c)<2 ? `${u.push(op)-1}@`:``)
     }
     else {
-      if (uop?.length) buf += `${u.push(uop)-1}@`
-      uop = null, i++
-      if (c=='('||c=='['||c=='{') { //group, call, array, prop, obj
-        if (buf) cur.push(buf), buf='';
-        (up=cur).push(cur=[c]), cur.up=up
-      }
-      else if (br[cur[0]]==c) {
-        if (buf) cur.push(buf), buf=''
-        cur=cur.up
-      }
-      else buf += c
+      if (op?.length) op=null
+      if (c=='('||c=='['||c=='{') g[cur[0]]+=`#${c}${cur.unshift(g.push('')-1)}`, op=[]
+      else if (c==')'||c==']'||c=='}') cur.shift()
+      else g[cur[0]]+=c, op=null
+      i++
     }
   }
-  if (buf) cur.push(buf)
 
   // binaries w/precedence
-  // FIXME: create tree instead of groups, convert ops in-place
+  // FIXME: create tree instead of groups, convert opf in-place
   const oper = (s, l) => Array.isArray(s) ? [s.shift(), ...s.map(oper)] : s.includes(op) ? [op, ...s.split(op)] : s.trim()
-  for (op of opl) cur = cur.map(oper)
+  for (op of opl) g = g.map(oper)
 
   // unwrap
-//FIXME: rebuild tree here
-  const deref = (s,c,e,r,i,m,n,un,a) => Array.isArray(s) ? [s.shift(), ...s.map(deref)]
-    : ~(c = s.indexOf('#')) ? (
-      e=s.indexOf('#',c+2), e = ~e?e:s.length,
-      i = s.slice(c+2,e), m=s[c+1],
-      n=s.slice(un=s.indexOf('@')+1, c),
-      // k=s.substr(0,  s.indexOf(':')),
-      r = m=='v' ? console.log(s,c)||v[i]
-        : m == 'g' ? (a=deref(g[i]), c ? [n,a] : a) // fn call or group
-        : m == 'p' ? (a=deref(g[i]), c ? ['[',n,a] : a) // property or array
-        : m == 'o' ? !g[i]?{}:(a=g[i][0]==':'?[g[i]]:g[i].slice(1), Object.fromEntries(a.map(x=>[unq(deref(x[1])),deref(x[2])])))
-        : r, // id
-      r = un ? u[s.slice(0,un-1)].reduce((r,op)=>[op, r],r) : r // unary op
-    )
-    : s
+  const deref = (s,c,e,r,i,m,n,ui,a,uop) => {
+    if (Array.isArray(s)) return [s.shift(), ...s.map(deref)]
+    if (~(ui=s.indexOf('@'))) {
+      uop = u[s.slice(0,ui)]
+      s = s.slice(ui+1)
+    }
+    while (s[0]=='#') {
+      m = s[1]
+      s = s.slice(2)
+      if (m=='v') s=v[s]
+    }
+    if (uop) s = uop.reduce((s,uop)=>[uop, s],s)
+    // ~(c = s.indexOf('#')) ? (
+    //   e=s.indexOf('#',c+2), e = ~e?e:s.length,
+    //   i = s.slice(c+2,e), m=s[c+1],
+    //   n=s.slice(un=s.indexOf('@')+1, c),
+    //   // k=s.substr(0,  s.indexOf(':')),
+    //   r = m=='v' ? console.log(s,c)||v[i]
+    //     // : m == 'g' ? (a=deref(g[i]), c ? [n,a] : a) // fn call or group
+    //     // : m == 'p' ? (a=deref(g[i]), c ? ['[',n,a] : a) // property or array
+    //     // : m == 'o' ? !g[i]?{}:(a=g[i][0]==':'?[g[i]]:g[i].slice(1), Object.fromEntries(a.map(x=>[unq(deref(x[1])),deref(x[2])])))
+    //     : r, // id
+    //   r = un ? u[s.slice(0,un-1)].reduce((r,op)=>[op, r],r) : r // unary op
+    // )
+    return s
+  }
 
-  return deref(cur)
+  return deref(g[0])
 }
 const unq = s => s[0]=='"'?s.slice(1,-1):s
 
 // tree → result
-export const evaluate = (seq, ctx={}, ops=operators, f,k) => Array.isArray(seq)
-// FIXME: possibly we have to reduce non-pairs but full ops
-  ? (f=ops[seq[0]]||ctx[seq[0]],seq=seq.slice(1).map(x=>console.log(x,evaluate(x,ctx,ops))||evaluate(x,ctx,ops)), seq.length<2 ? f(void 0,seq[0]) : seq.reduce(f))
+export const evaluate = (seq, ctx={}, opf=operators, f,k) => Array.isArray(seq)
+// FIXME: possibly we have to reduce non-pairs but full opf
+  ? (f=opf[seq[0]]||ctx[seq[0]],seq=seq.slice(1).map(x=>console.log(x,evaluate(x,ctx,opf))||evaluate(x,ctx,opf)), seq.length<2 ? f(void 0,seq[0]) : seq.reduce(f))
   : typeof seq === 'string' ? (seq[0] === '"' ? seq.slice(1,-1) : ctx[seq])
   : seq
 
 // code → evaluator
-export default (seq, ops=operators) => (
-  seq = typeof seq === 'string' ? parse(seq, ops) : seq,
-  ctx => evaluate(seq, ctx, ops)
+export default (seq, opf=operators) => (
+  seq = typeof seq === 'string' ? parse(seq, opf) : seq,
+  ctx => evaluate(seq, ctx, opf)
 )
 
 const dprop = (obj, key)=> key.split('.').reduce((a,b)=>b?a?.[b]:a, obj)
--- todo.md
+++ todo.md
@@ -6,8 +6,25 @@
   ? Array vs fn call
     1. `Array → [['a','b','c']], call → ['a', 'b', 'c']` (frisk)
       + escape any value by just [{}], [[]], [Date] ...
+      - [[a,1],2] → a(1)(2), therefore [[a,1]] → a(1)()
     2. `Array → ['list', 'a', 'b', 'c'], call → ['a', 'b', 'c']` (lisp)
+      + → list(a,b,c)
+      - list can be external function that we supercede
+    2.1 `['Array', 'a', 'b', 'c']`
+      + → Array(a,b,c)
+        + lucky match w/o new operator
+      - `[1,2,3] → ['Array',[',',1,,2,3]]`, which is redundant
+      2.1.1 ✔ `[Array, 'a', 'b', 'c']`
+        + since our notation implies fn call, and first arg is fn name, operator or token to call, then we just call that token
+    2.2 `['...', 'a', 'b', 'c']`
+      + unary operator generates list anyways
+      - can be super confusing with real ... operator, like `...[a,b,c] → ['...',['...', a,b,c]]`
+    2.3 `['[', 1, 2, 3]`
+      + keeps reference to operator
+      + `[` as prop access is transfused into `.`
+      - still makes nested ',' - suppose comma should not create separate group `[',', a, b, c]` but should instead just push arguments to current
     3. `Array → [,'a','b','c']`, call → ['a', 'b', 'c']
+      - undefined(a,b,c)
     4. `Array → ['a', 'b', 'c'], call → ['a', 'b', 'c'] if 'a' is a function` (frisk)
 * [ ] word operators
 * [ ] subscript`exp`
