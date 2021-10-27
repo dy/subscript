583a122540733a48e43d15e0cc91a3669af99c43
dy <df.creative@gmail.com>
2021-10-26 20:17:55 -0400

    Narrow down fn problem

--- subscript.js
+++ subscript.js
@@ -15,7 +15,7 @@ export const precedence = [
   // '**',
   // ['!','~','+','-','++','--']
   '!',
-  ['.', '('],
+  // ['.', '('],
 ]
 
 // FIXME: operators must take full args, not just pair: to directly map lisp constructs [op, ...args], not some reduce shchema
@@ -73,8 +73,11 @@ export function parse (seq) {
     // FIXME: is it possible to enable recursion somehow? seems like group refs could be solved simpler
     // FIXME: ideally we'd merge it with psplit: make this first run is for (, [, . operators and call psplit from within, not after
     else if (c=='('||c=='[') {
+      // NOTE: we have to handle .[(, specially: besides parsing complexity we still need special folding for them, also it comes before precedence order
+      // a(b) → [a,b];     a(b,c)(d) → [[a, b, c], d]
+      // (a) → a;          (a,b) → [,a,b] or [',',a,b]
       ref=g.push('')-1,
-      g[cur[0]] += b + (c=='['?`.`: /\w$/.test(b) ?`(`:``) + `#g${ref}` // FIXME: this is redundant regex that's algorithm defect
+      g[cur[0]] += b + (c=='['?`.`: `@(`) + `#g${ref}`
       cur.unshift(ref),
       op=[], i++, b=''
     }
@@ -91,17 +94,12 @@ export function parse (seq) {
 
   // unwrap
   const deref = s => {
-    let c,un
-    if (Array.isArray(s)) return s
-      .map(deref)
-      .filter(x=>x!=nil) // [[a,nil]] → [[a]]
+    let c
+    if (Array.isArray(s)) return s.map(deref)
+      // .filter(x=>x!=nil) // [[a,nil]] → [[a]]
       // FIXME: here is a conflict of a(1,2,3) vs +(1,2,3)
       // ? possibly we need to avoid , operator and split , here... but not much difference with whan we have now.
-      // ? or split in unp, where we know that a is not an operator... we don't have deref there
-      // ? alternatively we combine deref and unp, eg. deref in unp... it's getting messy as if we do something wrong.
-      // ? alternatively just drop these attempts to fit calls and directly parse as \w+(.*)
     if (s[0]=='#') c=s.slice(2), s = s[1]=='v'?v[c]:!g[c]?nil:deref(g[c])
-    if (un) s = un.reduce((s,u)=>[u,s],s)
     return s
   }
 
@@ -117,42 +115,31 @@ const psplit = (s, ops) => {
     if (~(c=ops.indexOf(op=s[i]+s[i+1])) || ~(c=ops.indexOf(op=s[i]))) {
       if (i>i0) {
         if (!cur) cur = [op]
-        cur.push(un.reduce((t,u)=>[u,t], s.slice(i0,i))) // 1 + 2 + 3 → [+,1,2,3]
+        cur.push(un.reduce((t,u)=>[u,t], s.slice(i0,i))) // 1+2+3 → [+,1,2,3]
         un = []
-        if (op != cur[0]) cur=[op, cur] // 1 + 2 - 3 → [-, [+, 1, 2], 3]
+        if (op != cur[0]) cur=[op, cur] // 1+2-3 → [-,[+, 1, 2],3]
       }
       else un.push(op)
       i0=i+op.length
     }
   }
   if (!cur) return s
-  // FIXME: it there a way to remove duplicate
+  // FIXME: it there a way to remove duplicate?
   if (i>i0) cur.push(un.reduce((t,u)=>[u,t], s.slice(i0,i)))
   return cur
 }
 
 // calltree → result
 export const evaluate = (seq, ctx={}, f,k) => Array.isArray(seq)
-// FIXME: possibly we have to reduce non-pairs but full opf
-  ? (f=operators[seq[0]]||ctx[seq[0]], seq=seq.slice(1).map(x=>seq.length<2 ? f(void 0,seq[0]) : seq.reduce(f)))
+// FIXME: possibly we have to reduce not pairs but full args
+  ? (f=ctx[seq[0]]||operators[seq[0]], seq=seq.slice(1).map(x=>seq.length<2 ? f(void 0,seq[0]) : seq.reduce(f)))
   : typeof seq === 'string' ? (seq[0] === '"' ? seq.slice(1,-1) : ctx[seq])
   : seq
 
 // code → evaluator
 export default (seq) => (seq = typeof seq === 'string' ? parse(seq) : seq, ctx => evaluate(seq, ctx))
 
-// // operator groups
-// const opx = (s, op) => Array.isArray(s) ? [s.shift(), ...s.map(s=>opx(s,op))]
-//     : s.includes(op) ? [op, ...s.split(op)]
-//     : s
-
 // const dprop = (obj, key)=> key.split('.').reduce((a,b)=>b?a?.[b]:a, obj)
 
-// // call chains
-// const unp = (s) => Array.isArray(s) ? [s.shift(), ...s.map(unp)] :
-//     s.includes('(') ?
-//     // a(#1).b(#2) → [a,#1,b,#2 → [.,[a,...#1],b],  a(#1)(#2) → [a,#1,,#2 → [[a,...#1],b]
-//     s.split(/\(|\)/).reduce((a,b,i) => i%2 ? [opx(a,'.'),b] : b ? ['.',a,...b.slice(1).split('.')] : a)
-//     : opx(s,'.')
 // // remove quote
 // const unq = s => s[0]=='"'?s.slice(1,-1):s
