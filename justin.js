3687fbfdd0b99f4c278d33a79281f32ddee2e6c5
dy <df.creative@gmail.com>
2021-11-02 12:01:15 -0400

    Complete justin

--- justin.js
+++ justin.js
@@ -1,5 +1,5 @@
 // justin lang https://github.com/endojs/Jessie/issues/66
-import sub, {operators, transforms, blocks, comments} from './subscript.js'
+import sub, {operators, transforms, blocks, comments, quotes} from './subscript.js'
 
 // **
 operators.splice(2,0,{'**': (...args)=>args.reduceRight((a,b)=>Math.pow(b,a))})
@@ -24,11 +24,27 @@ blocks['{']='}'
 comments['/*']='*/'
 comments['//']='\n'
 
-// in operator
+// in
 operators[5]['in'] = (a,b)=>a in b
 transforms['in'] = n => (n[1]=`"${n[1]}"`, n)
 
-// TODO: `...x` unary operator
+// {}, []
+blocks['{']='}'
+operators.unshift({'[':a=>Array(...a), '{':a=>Object.fromEntries(a)})
+transforms['['] = s => s.length > 2 ? s
+  : s[1] === undefined
+  ? [Array]
+  // ['[',[',',a,[',',b],c]] → ['[',[',',a,,b,c]]
+  : [Array].concat(isnode(s[1]) ? s[1].slice(1).reduce((a,b)=>a.push(...(isnode(b)&&b[0]==','?(b[0]=undefined,b):[b]))&&a,[])
+  : s[1])
+transforms['{'] = (s,entries) => {
+  if (s[1]===undefined) return {}
+  if (s[1][0]==':') entries = [s[1].slice(1)]
+  else entries = s[1].slice(1).map(n=>n.slice(1))
+  entries = entries.map(n=>quotes[n[0][0]]?[n[0].slice(1,-1),n[1]]:n)
+  return Object.fromEntries(entries)
+}
+
 // TODO: strings interpolation
 
 export * from './subscript.js'
