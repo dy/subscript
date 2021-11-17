2ae8b37d0802cace0b5797183f97a309be1b8401
dy <df.creative@gmail.com>
2021-11-16 22:01:18 -0500

    Fix all tests

--- subscript.js
+++ subscript.js
@@ -24,16 +24,15 @@ const code = () => cur.charCodeAt(index), // current char code
   },
 
   isCmd = a => Array.isArray(a) && (typeof a[0] === 'string' || isCmd(a[0])),
-  map = (node, t) => isCmd(node) ? (t = transforms[node[0]], t?t(node):node) : node
+  map = (node, t) => isCmd(node) ? (t = parse.map[node[0]], t?t(node):node) : node
 
 
-export const parse = expr => (cur=expr, index=0, group(lastOp = ['',0])),
-
+export const
 // `foo.bar(baz)`, `1`, `"abc"`, `(a % 2)`
 group = (curOp, rootEnd=end, curEnd) => {
   index += curOp[0].length // group always starts with an operator +-b, a(b, +(b, a+b+c, so we skip it
 
-  if (curEnd = groups[curOp[0]]) end = curEnd // also we write root end marker
+  if (curEnd = parse.group[curOp[0]]) end = curEnd // also we write root end marker
 
   space();
 
@@ -41,12 +40,13 @@ group = (curOp, rootEnd=end, curEnd) => {
 
   // parse node by token parsers
   // FIXME: maybe instead of just next it's worth exposing full parsing st (that can later be merged into class?)
-  tokens.find(token => (node = token()) !== '')
+  parse.token.find(token => (node = token()) !== '')
+  // if (node === '') (op = operator(parse.prefix)) && (node = map([op[0], group(op)]))
 
   space()
 
   // consume expression for current precedence or group (== highest precedence)
-  while ((op = operator(binary)) && (curEnd || op[1] > curOp[1])) {
+  while ((op = operator(parse.binary)) && (curEnd || op[1] > curOp[1])) {
     node = [op[0], node]
     // consume same-op group, that also saves op lookups
     while (char(op[0].length) === op[0]) node.push(group(op))
@@ -60,7 +60,7 @@ group = (curOp, rootEnd=end, curEnd) => {
   return node;
 },
 
-unary = op => (op = operator(prefix)) && map([op[0], group(op)]),
+unary = op => (op = operator(parse.prefix)) && map([op[0], group(op)]), // FIXME: consolidate into group
 
 float = (number, c, e, isDigit) => {
   const E = 69, _E = 101, PLUS = 43, MINUS = 45, PERIOD = 46
@@ -89,100 +89,67 @@ id = (node, isId, cc, sem=0) => {
   else if (node === 'null') return null
 
   // parse a.b.c props
-  const PERIOD = 46,
-        OPAREN = 40, // (
-        CPAREN = 41, // )
-        OBRACK = 91, // [
-        CBRACK = 93; // ]
+  // const PERIOD = 46,
+  //       OPAREN = 40, // (
+  //       CPAREN = 41, // )
+  //       OBRACK = 91, // [
+  //       CBRACK = 93; // ]
 
-  while (next(c => (c === PERIOD) && (cc=c, 1))) {
-    if (cc === PERIOD) node = ['.', node, '"' + next(isId) + '"']
-    // else if (cc === OBRACK) node = ['[', node].concat(group(']')||[])
-    // FIXME: this might be suboptimal
-    // else if (cc === OPAREN) node = [node, next(c => (c === CPAREN ? sem-- && true : c === OPAREN ? (sem++, true) : true ))]
-  }
+  // while (next(c => (c === PERIOD) && (cc=c, 1))) {
+  //   if (cc === PERIOD) node = ['.', node, '"' + next(isId) + '"']
+  //   // else if (cc === OBRACK) node = ['[', node].concat(group(']')||[])
+  //   // FIXME: this might be suboptimal
+  //   // else if (cc === OPAREN) node = [node, next(c => (c === CPAREN ? sem-- && true : c === OPAREN ? (sem++, true) : true ))]
+  // }
 
   return node
 },
 
-groups = {'(':')','[':']'}, // FIXME: consolidate under group
-tokens = [float, string, id, unary],
+parse = Object.assign(
+  expr => (cur=expr, index=0, group(lastOp = ['', -1])),
+  {
+    group: {'(':')','[':']'}, // FIXME: consolidate under group
+    token: [float, string, id, unary],
 
-prefix = {
-  '-': 10,
-  '!': 10,
-  '+': 10,
-  '(': 10,
-  '++': 10,
-  '--': 10
-},
-postfix = {
-  '++': 10,
-  '--': 10
-},
-binary = {
-  ',': 0,
-  '||': 1, '&&': 2, '|': 3, '^': 4, '&': 5,
-  '==': 6, '!=': 6,
-  '<': 7, '>': 7, '<=': 7, '>=': 7,
-  '<<': 8, '>>': 8, '>>>': 8,
-  '+': 9, '-': 9,
-  '*': 10, '/': 10, '%': 10,
-  '.': 11, '[': 11, '(': 11
-},
+    prefix: {
+      '-': 10,
+      '!': 10,
+      '+': 10,
+      '(': 10,
+      '++': 10,
+      '--': 10
+    },
+    postfix: {
+      '++': 10,
+      '--': 10
+    },
+    binary: {
+      ',': 0,
+      '||': 1, '&&': 2, '|': 3, '^': 4, '&': 5,
+      '==': 6, '!=': 6,
+      '<': 7, '>': 7, '<=': 7, '>=': 7,
+      '<<': 8, '>>': 8, '>>>': 8,
+      '+': 9, '-': 9,
+      '*': 10, '/': 10, '%': 10,
+      '.': 11, '[': 11, '(': 11
+    },
 
-// FIXME: ideally these should be merged into `token` - we could parse group/prop as single token, as jsperf does
-transforms = {
-  '(': n => n.length < 3 ? n[1] : n.slice(1).reduce(
-      (a,b)=>[a].concat(b==null ? [] : b[0]==',' ? b.slice(1).map(x=>x===''?undefined:x) : [b]),
-    ), // [(,a,args1,args2] → [[a,...args1],...args2]
-  '[': n => (n[0]='.',n),
-  // '.': n => ['.',n[1],...n.slice(2).map(s=>typeof s === 'string' ? '"'+s+'"' : s)] // [.,a,b] → [.,a,"b"]
-},
-
-// op evaluators
-// multiple args allows shortcuts, lisp compatible, easy manual eval, functions anyways take multiple arguments
-operators = {
-  '!':a=>!a,
-  '++':a=>++a,
-  '--':a=>--a,
-
-  '.':(...a)=>a.reduce((a,b)=>a?a[b]:a),
-  '(':(a,...args)=>a(...args),
-  '[':(a,...args)=>a[args.pop()],
-
-  '%':(...a)=>a.reduce((a,b)=>a%b),
-  '/':(...a)=>a.reduce((a,b)=>a/b),
-  '*':(...a)=>a.reduce((a,b)=>a*b),
-
-  '+':(...a)=>a.reduce((a,b)=>a+b),
-  '-':(...a)=>a.length < 2 ? -a : a.reduce((a,b)=>a-b),
-
-  '>>>':(a,b)=>a>>>b,
-  '>>':(a,b)=>a>>b,
-  '<<':(a,b)=>a<<b,
-
-  '>=':(a,b)=>a>=b,
-  '>':(a,b)=>a>b,
-  '<=':(a,b)=>a<=b,
-  '<':(a,b)=>a<b,
-
-  '!=':(a,b)=>a!=b,
-  '==':(a,b)=>a==b,
-
-  '&':(a,b)=>a&b,
-  '^':(a,b)=>a^b,
-  '|':(a,b)=>a|b,
-  '&&':(...a)=>a.every(Boolean),
-  '||':(...a)=>a.some(Boolean),
-  ',':(...a)=>a.reduce((a,b)=>(a,b))
-},
+    // FIXME: ideally these should be merged into `token` - we could parse group/prop as single token, as jsperf does
+    map: {
+      '(': n => n.length < 3 ? n[1] : n.slice(1).reduce(
+          (a,b)=>[a].concat(b==null ? [] : b[0]==',' ? b.slice(1).map(x=>x===''?undefined:x) : [b]),
+        ), // [(,a,args1,args2] → [[a,...args1],...args2]
+      '[': n => (n[0]='.',n),
+      '.': n => ['.',n[1],...n.slice(2).map(s=>typeof s === 'string' ? '"'+s+'"' : s)] // [.,a,b] → [.,a,"b"]
+    }
+  }
+),
 
 // calltree → result
-evaluate = (s, ctx={}, c, op) => {
+evaluate = Object.assign((s, ctx={}, c, op) => {
   if (isCmd(s)) {
     c = s[0]
-    if (typeof c === 'string') op = operators[c]
+    if (typeof c === 'string') op = evaluate.operator[c]
     c = op || evaluate(c, ctx) // [[a,b], c]
     if (typeof c !== 'function') return c
 
@@ -194,7 +161,45 @@ evaluate = (s, ctx={}, c, op) => {
           : s in ctx ? ctx[s] : s
 
   return s
-}
+}, {
+  // op evaluators
+  // multiple args allows shortcuts, lisp compatible, easy manual eval, functions anyways take multiple arguments
+  operator: {
+    '!':a=>!a,
+    '++':a=>++a,
+    '--':a=>--a,
+
+    '.':(...a)=>a.reduce((a,b)=>a?a[b]:a),
+    '(':(a,...args)=>a(...args),
+    '[':(a,...args)=>a[args.pop()],
+
+    '%':(...a)=>a.reduce((a,b)=>a%b),
+    '/':(...a)=>a.reduce((a,b)=>a/b),
+    '*':(...a)=>a.reduce((a,b)=>a*b),
+
+    '+':(...a)=>a.reduce((a,b)=>a+b),
+    '-':(...a)=>a.length < 2 ? -a : a.reduce((a,b)=>a-b),
+
+    '>>>':(a,b)=>a>>>b,
+    '>>':(a,b)=>a>>b,
+    '<<':(a,b)=>a<<b,
+
+    '>=':(a,b)=>a>=b,
+    '>':(a,b)=>a>b,
+    '<=':(a,b)=>a<=b,
+    '<':(a,b)=>a<b,
+
+    '!=':(a,b)=>a!=b,
+    '==':(a,b)=>a==b,
+
+    '&':(a,b)=>a&b,
+    '^':(a,b)=>a^b,
+    '|':(a,b)=>a|b,
+    '&&':(...a)=>a.every(Boolean),
+    '||':(...a)=>a.some(Boolean),
+    ',':(...a)=>a.reduce((a,b)=>(a,b))
+  }
+})
 
 // code → evaluator
 export default s => (s = typeof s == 'string' ? parse(s) : s,  ctx => evaluate(s, ctx))
