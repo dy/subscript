7995537f1c54edc7e9c4d3772d7de1df08607869
dy <df.creative@gmail.com>
2021-11-20 13:02:29 -0500

    Init parser scheme

--- parse.js
+++ parse.js
@@ -1,80 +1,44 @@
-const PERIOD = 46, OPAREN = 40, CPAREN = 41, OBRACK = 91, CBRACK = 93, PLUS = 43, MINUS = 45
+const GT = 62, LT = 60, EQ = 61, PLUS = 43, MINUS = 45, PIPE = 124
 
-export let index, current, lastOp, end
+// precedence-based parsing
+let index, current
 
-export const parse = (str, tree) => (current=str, index=lastOp=0, tree=expr(), index < current.length ? err() : tree),
+export const parse = (str, tree) => (current=str, index=0, expr()),
 
-// ------------ util
-code = () => current.charCodeAt(index), // current char code
-char = (n=1) => current.substr(index, n), // skip n chars
-err = (msg=char()) => { throw Error('Bad syntax ' + msg + ' at ' + index) },
-skip = (is=1, from=index) => { // consume N or until condition matches
-  if (typeof is === 'number') index += is
-  else while (is(code())) ++index > current.length && err(is) // 1 + true === 2;
-  return index > from ? current.slice(from, index) : null
+err = (msg='Bad syntax '+char()) => { throw Error(msg + ' at ' + index) },
+skip = (is=1, from=index) => {
+  if (typeof is === 'number') index += is else while (is(code())) index++;
+  return current.slice(from, index)
 },
-space = () => { while (code() <= 32) index++ },
+space = cc => { while (cc = code(), cc < 33) index++; return cc },
 
-// ------------- expr
-// consume operator
-operator = (ops, op, prec, l=3) => {
-  // memoize by index - saves 20% to perf
-  if (index && lastOp && lastOp[3] === index) return lastOp
+code = (i=0) => current.charCodeAt(index+i),
+char = (n=1) => current.substr(index, n),
 
-  // ascending lookup is faster for 1-char operators, longer for 2+ char ops, so we use descending
-  while (l) if ((prec=ops[op=char(l--)])!=null) return lastOp = [op, prec, op.length, index] //opinfo
-},
+token = [float, string, literal, id],
 
-expr = (prec=-1, curEnd) => {
-  space()
+expr = (preс=0, cc, node, group, from=index) => {
+  cc = space()
 
-  let cc = code(), op, node, i=0, mapped, from=index, prevEnd
-  if (cc === end) return //shortcut
+  // prefix or token
+  while (from===index && i < token.length) node = node[i++](cc)
 
-  if (curEnd) prevEnd = end, end = curEnd // global end marker saves operator lookups
-
-  // parse node by token parsers (direct loop is faster than token.find)
-  while (from===index && i < token.length) node = token[i++](cc)
-
-  // unary prefix
-  if (from===index) (op = operator(unary)) && (index += op[2], node = [op[0], expr(op[1])])
-
-  // postfix handlers
-  else {
-    for (space(), cc=code(), i=0; i < postfix.length;)
-      if ((mapped = postfix[i](node, cc)) !== node) node = mapped, space(), cc=code(); else i++
-  }
-  // ALT: seems to be slower
-  // else do {space(), cc=code()} while (postfix.find((parse, mapped) => (mapped = parse(node, cc)) !== node && (node = mapped)))
-
-  // consume binary expression for current precedence or higher
-  while (cc = code() && (cc !== end) && (op = operator(binary)) && op[1] > prec) {
-    node = [op[0], node]
-    // consume same-op group, do..while both saves op lookups and space
-    do { index += op[2], node.push(expr(op[1])) } while (char(op[2]) === op[0])
-    space()
-  }
-
-  if (curEnd) end = code() !== curEnd ? err() : prevEnd
-
-  return node;
+  // postfix or binary
+  while (!(group = operator(node, prec, cc = space())));
 },
 
-// ------------------- tokens
+// --------- token
 // 1.2e+3, .5 - fast & small version, but consumes corrupted nums as well
 float = (number) => {
   if (number = skip(c => (c >= 48 && c <= 57) || c === PERIOD)) {
     if (code() === 69 || code() === 101) number += skip(2) + skip(c => c >= 48 && c <= 57)
-    return isNaN(number = parseFloat(number)) ? err() : number
+    return isNaN(number = parseFloat(number)) ? err('Bad number') : number
   }
 },
-
 // "a"
 string = (q, qc) => q === 34 ? (qc = char(), index++, qc) + skip(c => c-q) + (index++, qc) : null,
-
 // (...exp)
 group = (c, node) => c === OPAREN ? (index++, node = expr(-1,CPAREN), index++, node) : null,
-
 // var or literal
 id = name => (name = skip(c =>
   (
@@ -86,49 +50,118 @@ id = name => (name = skip(c =>
   )
 )),
 
-// ----------- config
-token = parse.token = [ float, group, string, id ],
+token = [ float, group, string, id ],
 
-literal = parse.literal = {true:true, false:false, null:null},
-
-postfix = parse.postfix = [
-  // postfix parsers merged into 1 for performance & compactness
-  (node, cc, arg) => {
-    // a.b[c](d)
-    if (cc === PERIOD) index++, space(), node = ['.', node, '"'+id()+'"']
-    else if (cc === OBRACK) index++, node = ['.', node, expr(-1,CBRACK)], index++
-    else if (cc === OPAREN)
-      index++, arg=expr(-1,CPAREN),
-      node = Array.isArray(arg) && arg[0]===',' ? (arg[0]=node, arg) : arg == null ? [node] : [node, arg],
-      index++
-
-    // a++, a--
-    else if ((cc===0x2b || cc===0x2d) && current.charCodeAt(index+1)===cc) node = [skip(2), node]
-
-    // literal
-    else if (typeof node === 'string' && literal.hasOwnProperty(node)) node = literal[node]
-
-    return node
-  }
-],
-
-unary = parse.unary = {
-  '-': 17,
-  '!': 17,
-  '+': 17,
-  '++': 17,
-  '--': 17
+// ------------- postfix
+operator = (node, prec, cc) => {
+  for (let i = prec, result; i < operators.length; i++) result = operators[i](cc)
 },
+// route = () => {
+//   let c1 = code(), c2 = code(1)
+//   // multichar op lookup redirect
+//   if (c1 === GT || c1 === LT) return expr(12)
+//   else if (c1 === PLUS || c1 === MINUS) return expr(14)
+//   else if (c1 === EQ) return expr(11)
+//   else if (c1 === PIPE) return expr(6)
+//   else {
+//     // TODO: lookup single-char opreator - likely useful to have a map of single-arg precedences
+//   }
+// },
+comma = c1 => {},
+ternary = c1 => {},
+some = (c1, c2) => {},
+every = (c1, c2) => {},
+or = c1 => {},
+xor = c1 => {},
+and = c1 => {},
+eq = (c1, c2) => {},
+comp = (c1, c2) => {},
+shift = (c1, c2, c3) => {},
+sum = (c1) => {},
+mult = (c1) => {},
+unary = (c1, c2) => {},
+prop = (c1, c2) => {},
 
-binary = parse.binary = {
-  ',': 1,
-  '||': 6, '&&': 7, '|': 8, '^': 9, '&': 10,
-  '==': 11, '!=': 11,
-  '<': 12, '>': 12, '<=': 12, '>=': 12,
-  '<<': 13, '>>': 13, '>>>': 13,
-  '+': 14, '-': 14,
-  '*': 15, '/': 15, '%': 15
-}
+operators = [
+  route,
+  comma, // ',': 1,
+  ,
+  ,
+  // '?:'
+  ternary,
+  ,
+  // '||': 6, '&&': 7, '|': 8, '^': 9, '&': 10,
+  some,
+  every,
+  or,
+  xor,
+  and,
+
+  // '==': 11, '!=': 11,
+  eq,
+
+  // '<': 12, '>': 12, '<=': 12, '>=': 12,
+  comp,
+
+  // '<<': 13, '>>': 13, '>>>': 13,
+  shift,
+
+  // '+': 14, '-': 14,
+  sum,
+
+  // '*': 15, '/': 15, '%': 15
+  mult,
+  ,
+  // '-': 17,
+  // '!': 17,
+  // '+': 17,
+  // '++': 17,
+  // '--': 17
+  unary,
+
+  // '()', '[]', '.': 18
+  prop
+]
 
 
-export default parse
+
+
+// postfix = parse.postfix = [
+//   // postfix parsers merged into 1 for performance & compactness
+//   (node, cc, arg) => {
+//     // a.b[c](d)
+//     if (cc === PERIOD) index++, space(), node = ['.', node, '"'+id()+'"']
+//     else if (cc === OBRACK) index++, node = ['.', node, expr(-1,CBRACK)], index++
+//     else if (cc === OPAREN)
+//       index++, arg=expr(-1,CPAREN),
+//       node = Array.isArray(arg) && arg[0]===',' ? (arg[0]=node, arg) : arg == null ? [node] : [node, arg],
+//       index++
+
+//     // a++, a--
+//     else if ((cc===0x2b || cc===0x2d) && current.charCodeAt(index+1)===cc) node = [skip(2), node]
+
+//     // literal
+//     else if (typeof node === 'string' && literal.hasOwnProperty(node)) node = literal[node]
+
+//     return node
+//   }
+// ],
+
+// unary = parse.unary = {
+//   '-': 17,
+//   '!': 17,
+//   '+': 17,
+//   '++': 17,
+//   '--': 17
+// },
+
+// binary = parse.binary = {
+//   ',': 1,
+//   '||': 6, '&&': 7, '|': 8, '^': 9, '&': 10,
+//   '==': 11, '!=': 11,
+//   '<': 12, '>': 12, '<=': 12, '>=': 12,
+//   '<<': 13, '>>': 13, '>>>': 13,
+//   '+': 14, '-': 14,
+//   '*': 15, '/': 15, '%': 15
+// }
+
