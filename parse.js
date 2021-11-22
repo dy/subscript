0bb6c4607cebd5b84642f61a81e97d79c46bdb8e
dy <df.creative@gmail.com>
2021-11-22 14:14:12 -0500

    Pass end as argument

--- parse.js
+++ parse.js
@@ -18,18 +18,14 @@ code = (i=0) => cur.charCodeAt(idx+i),
 char = (n=1) => cur.substr(idx, n),
 
 // a + b - c
-expr = (arg=0, cc=space(), node, from=idx, i=0, mapped, prec, end) => {
+expr = (prec=0, end, cc=space(), node, from=idx, i=0, mapped) => {
   // prefix or token
   while (from===idx && i < token.length) node = token[i++](cc)
-
-  // prec may have end character in hi orders
-  // FIXME: better make a separate argument
-  if ((arg&0xff)>SPACE) end=arg, prec=0; else end=(prec&0xff00)>>8, prec=arg
 
   // postfix or binary
   for (cc=space(), i=prec; i < operator.length;)
     if (cc===end) break
-    else if (mapped = operator[i++](node, cc, i|end<<8)) node = mapped, i=prec, cc=space(); // we pass i+1 as precision
+    else if (mapped = operator[i++](node, cc, i, end)) node = mapped, i=prec, cc=space(); // we pass i+1 as precision
 
 
   return node
@@ -46,7 +42,7 @@ float = (number) => {
 // "a"
 string = (q, qc) => q === 34 ? (qc = char(), idx++, qc) + skip(c => c-q) + (idx++, qc) : '',
 // (...exp)
-group = (c, node) => c === OPAREN ? (idx++, node = expr(CPAREN), idx++, node) : '',
+group = (c, node) => c === OPAREN ? (idx++, node = expr(0,CPAREN), idx++, node) : '',
 // var or literal
 id = name => skip(c =>
   (
@@ -65,35 +61,35 @@ token = [ float, group, string, id ],
 // FIXME: seems that we have to consume same-level operators. That speeds up groups, as well as resolves unary issue.
 operator = [
   // ',': 1,
-  (a,cc,prec) => cc===COMMA ? [skip(),a,expr(prec)] : null,
+  (a,cc,prec,end) => cc===COMMA ? [skip(),a,expr(prec,end)] : null,
   // '||': 6, '&&': 7,
-  (a,cc,prec) => cc===OR&&code(1)===cc ? [skip(),a,expr(prec)] : null,
-  (a,cc,prec) => cc===AND&&code(1)===cc ? [skip(),a,expr(prec)] : null,
+  (a,cc,prec,end) => cc===OR&&code(1)===cc ? [skip(),a,expr(prec,end)] : null,
+  (a,cc,prec,end) => cc===AND&&code(1)===cc ? [skip(),a,expr(prec,end)] : null,
   // '|': 8, '^': 9, '&': 10,
-  (a,cc,prec) => cc===OR ? [skip(),a,expr(prec)] : null,
-  (a,cc,prec) => cc===HAT ? [skip(),a,expr(prec)] : null,
-  (a,cc,prec) => cc===AND ? [skip(),a,expr(prec)] : null,
+  (a,cc,prec,end) => cc===OR ? [skip(),a,expr(prec,end)] : null,
+  (a,cc,prec,end) => cc===HAT ? [skip(),a,expr(prec,end)] : null,
+  (a,cc,prec,end) => cc===AND ? [skip(),a,expr(prec,end)] : null,
   // '==': 11, '!=': 11,
-  (a,cc,prec) => cc===EQ&&cc===code(1) ? [skip(),a,expr(prec)] : null,
+  (a,cc,prec,end) => cc===EQ&&cc===code(1) ? [skip(),a,expr(prec,end)] : null,
   // '<': 12, '>': 12, '<=': 12, '>=': 12,
-  (a,cc,prec) => cc===GT||cc===LT ? [skip(),a,expr(prec)] : null,
+  (a,cc,prec,end) => cc===GT||cc===LT ? [skip(),a,expr(prec,end)] : null,
   // '<<': 13, '>>': 13, '>>>': 13,
-  (a,cc,prec) => (cc===LT||cc===GT)&&cc===code(1) ? [skip(cc===code(2)?3:2), a, expr(prec)] : null,
+  (a,cc,prec,end) => (cc===LT||cc===GT)&&cc===code(1) ? [skip(cc===code(2)?3:2), a, expr(prec,end)] : null,
   // '+': 14, '-': 14,
-  (a,cc,prec) => (cc===PLUS || cc===MINUS) && a!=='' && code(1) !== cc ? [skip(), a, expr(prec)] : null,
+  (a,cc,prec,end) => (cc===PLUS || cc===MINUS) && a!=='' && code(1) !== cc ? [skip(), a, expr(prec,end)] : null,
   // '*': 15, '/': 15, '%': 15
-  (a,cc,prec) => (cc===MUL && code(1) !== MUL) || cc===DIV || cc===MOD ? [skip(), a, expr(prec)] : null,
+  (a,cc,prec,end) => (cc===MUL && code(1) !== MUL) || cc===DIV || cc===MOD ? [skip(), a, expr(prec,end)] : null,
   // -- ++ unaries
-  (a,cc,prec) => (cc===PLUS || cc===MINUS) && code(1) === cc ? [skip(2), a===''?expr(prec):a] : null,
+  (a,cc,prec,end) => (cc===PLUS || cc===MINUS) && code(1) === cc ? [skip(2), a===''?expr(prec,end):a] : null,
   // - + ! unaries
-  (a,cc,prec) => (cc===PLUS || cc===MINUS || cc===EXCL)&&a==='' ? [skip(1), expr(prec)] : null,
+  (a,cc,prec,end) => (cc===PLUS || cc===MINUS || cc===EXCL)&&a==='' ? [skip(1), expr(prec,end)] : null,
   // '()', '[]', '.': 18
   (a,cc,prec,arg) => (
     // a.b[c](d)
     cc===PERIOD ? [skip(), a , '"'+(space(), id())+'"'] :
-    cc===OBRACK ? [(skip(),'.'), a, expr(CBRACK)] :
+    cc===OBRACK ? [(skip(),'.'), a, expr(0,CBRACK)] :
     cc===OPAREN ? (
-      idx++, arg=expr(CPAREN), idx++,
+      idx++, arg=expr(0,CPAREN), idx++,
       Array.isArray(arg) && arg[0]===',' ? (arg[0]=a, arg) :
       arg == null ? [a] :
       [a, arg]
