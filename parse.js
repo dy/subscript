2310702896152bdf5c1cbf7418b821007e634225
dy <df.creative@gmail.com>
2021-11-22 14:09:47 -0500

    Implement basic tests

--- parse.js
+++ parse.js
@@ -1,8 +1,8 @@
 // precedence-based parsing
-const GT=62, LT=60, EQ=61, PLUS=43, MINUS=45, AND=38, OR=124, HAT=94, MUL=42, DIV=47, MOD=37, PERIOD=46, OBRACK=91, CBRACK=93, OPAREN=40, CPAREN=41, COMMA=44
+const GT=62, LT=60, EQ=61, PLUS=43, MINUS=45, AND=38, OR=124, HAT=94, MUL=42, DIV=47, MOD=37, PERIOD=46, OBRACK=91, CBRACK=93, OPAREN=40, CPAREN=41, COMMA=44, SPACE=32, EXCL=33
 
 // current string & index
-let idx, cur
+let idx, cur, end
 
 export const parse = (str, tree) => (cur=str, idx=0, expr()),
 
@@ -12,21 +12,25 @@ skip = (is=1, from=idx) => {
   else while (is(code())) idx++;
   return cur.slice(from, idx)
 },
-space = cc => { while (cc = code(), cc < 33) idx++; return cc },
+space = cc => { while (cc = code(), cc <= SPACE) idx++; return cc },
 
 code = (i=0) => cur.charCodeAt(idx+i),
 char = (n=1) => cur.substr(idx, n),
 
-//a + b
-expr = (prec=0, cc=space(), node, from=idx, i=0, mapped) => {
+// a + b - c
+expr = (arg=0, cc=space(), node, from=idx, i=0, mapped, prec, end) => {
   // prefix or token
   while (from===idx && i < token.length) node = token[i++](cc)
 
-  if (!node) err('Unknown token')
+  // prec may have end character in hi orders
+  // FIXME: better make a separate argument
+  if ((arg&0xff)>SPACE) end=arg, prec=0; else end=(prec&0xff00)>>8, prec=arg
 
   // postfix or binary
   for (cc=space(), i=prec; i < operator.length;)
-    if (mapped = operator[i](node, cc, i)) node = mapped, i=prec, cc=space(); else i++
+    if (cc===end) break
+    else if (mapped = operator[i++](node, cc, i|end<<8)) node = mapped, i=prec, cc=space(); // we pass i+1 as precision
+
 
   return node
 },
@@ -40,11 +44,11 @@ float = (number) => {
   }
 },
 // "a"
-string = (q, qc) => q === 34 ? (qc = char(), idx++, qc) + skip(c => c-q) + (idx++, qc) : null,
+string = (q, qc) => q === 34 ? (qc = char(), idx++, qc) + skip(c => c-q) + (idx++, qc) : '',
 // (...exp)
-group = (c, node) => c === OPAREN ? (idx++, node = expr(-1,CPAREN), idx++, node) : null,
+group = (c, node) => c === OPAREN ? (idx++, node = expr(CPAREN), idx++, node) : '',
 // var or literal
-id = name => (name = skip(c =>
+id = name => skip(c =>
   (
     (c >= 48 && c <= 57) || // 0..9
     (c >= 65 && c <= 90) || // A...Z
@@ -52,48 +56,47 @@ id = name => (name = skip(c =>
     c == 36 || c == 95 || // $, _,
     c >= 192 // any non-ASCII
   )
-)),
-
+),
 token = [ float, group, string, id ],
 
 // operators
 // FIXME: check if binary op constructor affects performance anyhow, if not - just build condition-based
-// FIXME: likely most part of these can be consolidated to single checkers, isn't that? Like, too bad to insert anything between or/xor/and or between comparisons. Like prop/parens.
-// can't consolidate different precedences fns due to next-precedence call, can we?
 // FIXME: unary prefixes can come here as well: they just check if a is null
-
+// FIXME: seems that we have to consume same-level operators. That speeds up groups, as well as resolves unary issue.
 operator = [
   // ',': 1,
-  (a,cc,prec) => cc===COMMA?[skip(),a,expr(++prec)]:null,
+  (a,cc,prec) => cc===COMMA ? [skip(),a,expr(prec)] : null,
   // '||': 6, '&&': 7,
-  (a,cc,prec) => cc===OR&&code(1)===cc?[skip(),a,expr(++prec)]:null,
-  (a,cc,prec) => cc===AND&&code(1)===cc?[skip(),a,expr(++prec)]:null,
+  (a,cc,prec) => cc===OR&&code(1)===cc ? [skip(),a,expr(prec)] : null,
+  (a,cc,prec) => cc===AND&&code(1)===cc ? [skip(),a,expr(prec)] : null,
   // '|': 8, '^': 9, '&': 10,
-  (a,cc) => cc===OR?[skip(),a,expr(++prec)]:null,
-  (a,cc) => cc===HAT?[skip(),a,expr(++prec)]:null,
-  (a,cc) => cc===AND?[skip(),a,expr(++prec)]:null,
+  (a,cc,prec) => cc===OR ? [skip(),a,expr(prec)] : null,
+  (a,cc,prec) => cc===HAT ? [skip(),a,expr(prec)] : null,
+  (a,cc,prec) => cc===AND ? [skip(),a,expr(prec)] : null,
   // '==': 11, '!=': 11,
-  (cc,prec) => cc===EQ&&cc===code(1)?[skip(),a,expr(++prec)]:null,
+  (a,cc,prec) => cc===EQ&&cc===code(1) ? [skip(),a,expr(prec)] : null,
   // '<': 12, '>': 12, '<=': 12, '>=': 12,
-  (cc,prec) => cc===GT||cc===LT?[skip(),a,expr(++prec)]:null,
+  (a,cc,prec) => cc===GT||cc===LT ? [skip(),a,expr(prec)] : null,
   // '<<': 13, '>>': 13, '>>>': 13,
-  (cc,prec,c3) => (cc===LT||cc===GT)&&cc===code(1) ? [skip(cc===code(2)?3:2), a, expr(++prec)] : null,
+  (a,cc,prec) => (cc===LT||cc===GT)&&cc===code(1) ? [skip(cc===code(2)?3:2), a, expr(prec)] : null,
   // '+': 14, '-': 14,
-  (a,cc,prec) => (cc===PLUS || cc===MINUS) && code(1) !== cc ? [skip(), a, expr(++prec)] : null,
+  (a,cc,prec) => (cc===PLUS || cc===MINUS) && a!=='' && code(1) !== cc ? [skip(), a, expr(prec)] : null,
   // '*': 15, '/': 15, '%': 15
-  (a,cc,prec) => (cc===MUL && code(1) !== MUL) || cc===DIV || cc===MOD ? [skip(), a, expr(++prec)] : null,
-  // - + -- ++ !
-  (a,cc,prec) => (cc===PLUS || cc===MINUS) ? [skip(2), node] : null,
+  (a,cc,prec) => (cc===MUL && code(1) !== MUL) || cc===DIV || cc===MOD ? [skip(), a, expr(prec)] : null,
+  // -- ++ unaries
+  (a,cc,prec) => (cc===PLUS || cc===MINUS) && code(1) === cc ? [skip(2), a===''?expr(prec):a] : null,
+  // - + ! unaries
+  (a,cc,prec) => (cc===PLUS || cc===MINUS || cc===EXCL)&&a==='' ? [skip(1), expr(prec)] : null,
   // '()', '[]', '.': 18
-  (a,cc,prec) => (
+  (a,cc,prec,arg) => (
     // a.b[c](d)
-    cc===PERIOD ? [skip(), a , '"'+(space(),id())+'"'] :
-    cc===OBRACK ? [skip(), node, expr(CBRACK)] :
+    cc===PERIOD ? [skip(), a , '"'+(space(), id())+'"'] :
+    cc===OBRACK ? [(skip(),'.'), a, expr(CBRACK)] :
     cc===OPAREN ? (
       idx++, arg=expr(CPAREN), idx++,
-      Array.isArray(arg) && arg[0]===',' ? (arg[0]=node, arg) :
-      arg == null ? [node] :
-      [node, arg]
+      Array.isArray(arg) && arg[0]===',' ? (arg[0]=a, arg) :
+      arg == null ? [a] :
+      [a, arg]
     ) : null
   )
 ]
