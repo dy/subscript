9201c44b5f43cf86203046a16982d1bcf4ae3bd1
dy <df.creative@gmail.com>
2021-11-24 21:39:46 -0500

    Fix basic cases

--- parse.js
+++ parse.js
@@ -1,5 +1,10 @@
 // precedence-based parsing
-const GT=62, LT=60, EQ=61, PLUS=43, MINUS=45, AND=38, OR=124, HAT=94, MUL=42, DIV=47, MOD=37, PERIOD=46, OBRACK=91, CBRACK=93, OPAREN=40, CPAREN=41, COMMA=44, SPACE=32, EXCL=33
+const GT=62, LT=60, EQ=61, PLUS=43, MINUS=45, AND=38, OR=124, HAT=94, MUL=42, DIV=47, MOD=37, PERIOD=46, OBRACK=91, CBRACK=93, OPAREN=40, CPAREN=41, COMMA=44, SPACE=32, EXCL=33,
+
+  // TODO: take over precedences from MDN
+  PREC_COMMA=0, PREC_SOME=1, PREC_EVERY=2, PREC_OR=3, PREC_XOR=4, PREC_AND=5,
+  PREC_EQ=6, PREC_COMP=7, PREC_SHIFT=8, PREC_SUM=9, PREC_MULT=10, PREC_UNARY=11, PREC_CALL=12
+
 
 // current string & index
 let idx, cur
@@ -21,15 +26,12 @@ char = (n=1) => cur.substr(idx, n),
 nil = '',
 
 // a + b - c
-expr = (prec=0, cc=parse.space(), node, from=idx, i=0, end=0, map, newNode) => {
-  if (prec>SPACE) end=prec, prec=0
-
+expr = (prec=0, cc=parse.space(), node, from=idx, i=0, map, newNode) => {
   // prefix or token
   while (from===idx && i < parse.token.length) node = parse.token[i++](cc)
 
-  // FIXME: end is safe to get rid of, if we handle groups externally: lookup is super-cheap
   while (
-    (cc=parse.space()) !== end && (map = lookup[cc]) && (newNode = map(cc, node, prec))
+    (map = lookup[cc=parse.space()]) && (newNode = map(cc, node, prec))
   ) {
     // TODO: if map is null, throw error as unknown character maybe?
 
@@ -37,8 +39,8 @@ expr = (prec=0, cc=parse.space(), node, from=idx, i=0, end=0, map, newNode) => {
     if ((node = newNode).indexOf(nil) >= 0) err('Bad expression')
   }
 
-console.groupEnd()
-  if (end && cc!=end) err('Unclosed paren')
+  // TODO:
+  // if (end && cc!=end) err('Unclosed paren')
 
   return node
 },
@@ -59,7 +61,7 @@ token = parse.token = [
   // "a"
   (q, qc) => q === 34 ? (skip() + skip(c => c-q) + skip()) : nil,
   // (...exp)
-  c => c === OPAREN ? (++idx, c=expr(CPAREN), ++idx, c===nil?err():c) : nil,
+  c => c === OPAREN ? (++idx, c=expr(), ++idx, c===nil?err():c) : nil,
   // var
   c => skip(c =>
     (c >= 48 && c <= 57) || // 0..9
@@ -68,17 +70,13 @@ token = parse.token = [
     c == 36 || c == 95 || // $, _,
     (c >= 192 && c != 215 && c != 247) // any non-ASCII
   )
-]
+],
 
 // fast operator lookup table
-const lookup = [],
-
-// TODO: take over precedences from MDN
-PREC_COMMA=0, PREC_SOME=1, PREC_EVERY=2, PREC_OR=3, PREC_XOR=4, PREC_AND=5,
-PREC_EQ=6, PREC_COMP=7, PREC_SHIFT=8, PREC_SUM=9, PREC_MULT=10, PREC_UNARY=11, PREC_CALL=12
+lookup = parse.lookup = []
 
 // ,
-lookup[COMMA] = 0
+lookup[COMMA] = (c,node,prec) => !prec&&[skip(),node,expr()]
 
 // ||, |
 lookup[OR] = (c,node,prec) =>
@@ -104,23 +102,31 @@ lookup[LT] = lookup[GT] = (c,node,prec) =>
 
 // + ++ - --
 lookup[PLUS] = lookup[MINUS] = (c,node,prec) =>
-  ((node===nil||code(1)==c) && prec<PREC_UNARY && [skip(code(1)==c?2:1),node===nil?expr(PREC_UNARY):node]) ||
+  ((node===nil||code(1)==c) && prec<PREC_UNARY && [skip(code(1)==c?2:1),node===nil?expr(PREC_UNARY-1):node]) ||
   (prec<PREC_SUM && [skip(),node,expr(PREC_SUM)])
+
+// ! ~
+lookup[EXCL] = (c,node,prec) => (node===nil) && prec<PREC_UNARY && [skip(),expr(PREC_UNARY-1)]
 
 // * / %
 lookup[MUL] = lookup[DIV] = lookup[MOD] = (c,node,prec) => prec<PREC_MULT && [skip(),node,expr(PREC_MULT)]
 
 // a.b[c](d)
-// FIXME: mb worth moving to a token
 lookup[PERIOD] = lookup[OBRACK] = lookup[OPAREN] = (cc,a,prec,b) =>
-  cc==PERIOD ? [skip(), a, typeof (b = expr(prec)) === 'string' ? '"' + b + '"' : b] :
-  cc==OBRACK ? (idx++, a = ['.', a, expr(CBRACK)], idx++, a) :
-  cc==OPAREN ? (
-    idx++, b=expr(CPAREN), idx++,
-    Array.isArray(b) && b[0]===',' ? (b[0]=a, b) :
-    b === nil ? [a] :
-    [a, b]
-  ) : nil
+  prec<PREC_CALL && (
+    cc==PERIOD ? [skip(), a, typeof (b = expr(PREC_CALL)) === 'string' ? '"' + b + '"' : b] :
+    cc==OBRACK ? (idx++, a = ['.', a, expr()], idx++, a) :
+    cc==OPAREN ? (
+      idx++, b=expr(), idx++,
+      Array.isArray(b) && b[0]===',' ? (b[0]=a, b) :
+      b === nil ? [a] :
+      [a, b]
+    ) : null
+  )
+
+// endings just reset token
+// FIXME: do we need them? any operator can close the group...
+lookup[CBRACK] = lookup[CPAREN]
 
 
 
