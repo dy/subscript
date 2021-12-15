c4cbb5c903a3e3a26b45e2e5d5919a57e9e468f2
dy <df.creative@gmail.com>
2021-12-15 18:58:58 -0500

    Update subscript.js

--- subscript.js
+++ subscript.js
@@ -1,4 +1,4 @@
-import parse, {skip, expr, code, literals, operator, err} from './parse.js'
+import parse, {skip, literals, operator} from './parse.js'
 
 const PERIOD=46, OPAREN=40, CPAREN=41, CBRACK=93, SPACE=32,
 
@@ -7,7 +7,7 @@ PREC_EQ=9, PREC_COMP=10, PREC_SHIFT=11, PREC_SUM=12, PREC_MULT=13, PREC_UNARY=15
 
 literals.push(
   // 1.2e+3, .5 - fast & small version, but consumes corrupted nums as well
-  n => (n = skip(c => (c > 47 && c < 58) || c == PERIOD)) && (
+  n => (n = skip(c => c == PERIOD || c > 47 && c < 58)) && (
     skip(c => c == 69 || c == 101) && (n += skip(2) + skip(c => c >= 48 && c <= 57)),
     +n
   ),
@@ -16,7 +16,7 @@ literals.push(
 )
 
 for (let i = 0, list = [
-  ',', PREC_SEQ, (a,b)=>(a,b),
+  ',', PREC_SEQ, (a,b)=>(a&&a._$?a.push(b):((a=[a,b])._$=1))&&a,
 
   '|', PREC_OR, (a,b)=>a|b,
   '||', PREC_SOME, (a,b)=>a||b,
@@ -61,15 +61,12 @@ for (let i = 0, list = [
   ['[',']'], PREC_CALL, (a,b) => a[b],
 
   // a.b
-  // FIXME: these 2 - how
-  '.', PREC_CALL, (a,b) => a[b], (a,b) => (b=expr(PREC_CALL), ctx => fn(a(ctx),b())) :
+  '.', PREC_CALL, (a,b,ctx) => a(ctx)[b()],
 
   // a(b)
-  ['(',')'], PREC_CALL, (a,b) => (
-    b.map && b[0]===',' ? (b[0]=node, b) : b ? [node, b||err()] : [node]
-  ),
+  ['(',')'], PREC_CALL, (a,b,ctx) => b&&b._$ ? a(ctx).apply(ctx,b) : a(ctx)(b),
   // (a+b)
-  ['(',')'], PREC_GROUP, (a=0) => a
+  ['(',')'], PREC_GROUP, (a=0)=>a
 ]; i < list.length;) operator(list[i++], list[i++], list[i++])
 
 export { parse }
