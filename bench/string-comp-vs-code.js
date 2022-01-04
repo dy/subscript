6ef4ffd4d0e440c36dcb9b8890d288ba6a018a21
dy <df.creative@gmail.com>
2022-01-04 18:28:05 -0500

    Extend benchmark

--- bench/string-comp-vs-code.js
+++ bench/string-comp-vs-code.js
@@ -14,3 +14,16 @@ for (let i = 0; i < MAX; i++) {
   for (let j = 0; j < b.length; j++) code(j) === b.charCodeAt(j)
 }
 console.timeEnd('code by code')
+
+
+console.time('[]')
+for (let i = 0; i < MAX; i++) {
+  a[0]==='a' && a[1]==='b' && a[2]==='c'
+}
+console.timeEnd('[]')
+
+console.time('charCodeAt')
+for (let i = 0; i < MAX; i++) {
+  a.charCodeAt(0)===97 && a.charCodeAt(1)===98 && a.charCodeAt(2)===99
+}
+console.timeEnd('charCodeAt')
