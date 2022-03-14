a41de33901a462ba15a4ff45a9faa45a411f672f
dy <df.creative@gmail.com>
2022-03-14 16:22:13 -0400

    Improve error token

--- parse.js
+++ parse.js
@@ -8,8 +8,8 @@ parse = s => (idx=0, cur=s, s = expr(), cur[idx] ? err() : s || ''),
 
 // err = (msg='Bad syntax',c=cur[idx]) => { throw SyntaxError(msg + ' `' + c + '` at ' + idx) },
 
-err = (msg='Bad syntax',c=cur[idx],prev=cur.slice(0,idx).split('\n'),last=prev.pop()) => {
-  throw SyntaxError(`${msg} \`${last.slice(-10)}${c}\` at ${prev.length}:${last.length}`)
+err = (msg='Bad syntax',frag=cur.slice(idx-10,idx+1),prev=cur.slice(0,idx).split('\n'),last=prev.pop()) => {
+  throw SyntaxError(`${msg} \`${frag}\` at ${prev.length}:${last.length}`)
 },
 
 skip = (is=1, from=idx, l) => {
