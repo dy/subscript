4eca1d050acc7b0c642c0bdbea9db37f8b6bbd1e
Dmitry Iv <df.creative@gmail.com>
2024-06-15 15:27:07 -0400

    Add not throwing ternary

--- feature/ternary.js
+++ feature/ternary.js
@@ -1,7 +1,10 @@
-import { token, expr } from '../src/parse.js'
+import { token, expr, next } from '../src/parse.js'
 import { operator, compile } from '../src/compile.js'
 import { PREC_ASSIGN, COLON } from '../src/const.js'
 
 // ?:
 token('?', PREC_ASSIGN, (a, b, c) => a && (b = expr(PREC_ASSIGN - 0.5, COLON)) && (c = expr(PREC_ASSIGN - 0.5), ['?', a, b, c]))
+// ALT: not throwing
+// token('?', PREC_ASSIGN, (a, b, c) => a && (b = expr(PREC_ASSIGN - 0.5)) && next(c => c === COLON) && (c = expr(PREC_ASSIGN - 0.5), ['?', a, b, c]))
+
 operator('?', (a, b, c) => (a = compile(a), b = compile(b), c = compile(c), ctx => a(ctx) ? b(ctx) : c(ctx)))
